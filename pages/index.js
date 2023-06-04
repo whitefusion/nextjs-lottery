e98d7849a96e6561c905f4499e1e610abe002d1a
xinbai <whitefusion@163.com>
2023-06-04 20:18:10 +0800

    fix: build

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import { Inter } from 'next/font/google'
 import { Header } from '../components/Header';
 import { LotteryEntrance } from '../components/LotteryEntrance';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-start p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-start p-24`}
     >
-      {/* <ManualHeader /> */}
       <Header />
       <LotteryEntrance />
     </main>
